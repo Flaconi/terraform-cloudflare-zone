1ec9120b1654a2bb056fbb99c38a28f7f0373e33
Sergey Novikov <sergey.novikov@flaconi.de>
2023-12-06 13:36:35 +0100

    OPS-5741 Fix fmt

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "cloudflare_zone" "this" {
 resource "cloudflare_zone_settings_override" "this" {
   zone_id = cloudflare_zone.this.id
   settings {
-    brotli         = var.settings_override.brotli
-    early_hints    = var.settings_override.early_hints
+    brotli      = var.settings_override.brotli
+    early_hints = var.settings_override.early_hints
     # Some settings are not available for "free" commercial plan
     http2          = cloudflare_zone.this.plan != "free" ? var.settings_override.http2 : null
     image_resizing = cloudflare_zone.this.plan != "free" ? var.settings_override.image_resizing : null
